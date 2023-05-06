00a061e2ae475a2aae10f010f5e6fd7715685edd
Vajmir <sassiffy1004@gmail.com>
2023-05-06 13:16:19 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,12 +7,12 @@ export default function Home() {
       <Head>
         <title>Bemajster.</title>
       </Head>
-      <main className="bg-blue-950 text-white w-screen h-screen grid grid-cols-3">
-        <div className="h-full w-full align-middle flex items-center">
+      <main className="bg-blue-950 text-white w-screen h-screen grid grid-flow-cols grid-cols-2 xl:grid-cols-3 overflow-hidden">
+        <div className="h-full xl:w-full align-middle flex items-center xl:justify-start justify-end">
           <span className={BN.className}>
             <div
-              className="flex flex-col text-5xl gap-4 p-20 leading-relaxed hover:leading-loose 
-                   hover:p-[7.5rem] transition-all duration-300 ease-out"
+              className="flex flex-col text-5xl gap-4 pr-20 xl:pl-20 leading-relaxed hover:leading-loose 
+                    hover:pr-[7.5rem] xl:hover:pl-[7.5rem] transition-all duration-300 ease-out"
             >
               <div className="hover-underline cursor-pointer">About me</div>
               <div className="hover-underline cursor-pointer">Projects</div>
@@ -20,12 +20,12 @@ export default function Home() {
             </div>
           </span>
         </div>
-        <div className="h-full w-full flex justify-center items-center content-center text-center align-middle">
+        <div className="h-full w-full flex justify-start xl:justify-center items-center content-center text-center align-middle">
           <div className="flex text-9xl select-none">
             <span className={BN.className}>Bemajster.</span>
           </div>
         </div>
-        <div className="h-full w-full"></div>
+        <div className="h-full w-full hidden xl:block"></div>
       </main>
     </>
   )
