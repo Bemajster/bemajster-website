e693c62e6ce3f4b5879426a79abc44ddeec13a95
Vajmir <sassiffy1004@gmail.com>
2023-05-06 14:20:31 +0200

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,12 +9,12 @@ export default function Navbar() {
           className="flex flex-col text-5xl gap-4 pl-20 leading-relaxed hover:leading-loose 
                     hover:pl-[7.5rem] transition-all duration-300 ease-out"
         >
-          <div className="hover-underline cursor-pointer">
-            <Link href="/">Home</Link>
-          </div>
-          <div className="hover-underline cursor-pointer">
-            <Link href="/about">About me</Link>
-          </div>
+          <Link href="/">
+            <div className="hover-underline cursor-pointer">Home</div>
+          </Link>
+          <Link href="/about">
+            <div className="hover-underline cursor-pointer">About me</div>
+          </Link>
           <div className="hover-underline cursor-pointer">Projects</div>
           <div className="hover-underline cursor-pointer">Articles</div>
         </div>
